190d6f9c7565d427cb8bf0f8236fe6239573811a
Solvin Shrestha <shresthasolvin@gmail.com>
2022-03-06 04:55:48 -0800

    Added rest and GraphQL API support

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,6 +1,8 @@
 import React, {useState} from 'react';
 import Class from "../Class/class.js";
+import ClassGraphQL from '../Class/ClassGraphQL.js';
 import "./home.css"
+
 
 function Home(props) {
 
@@ -31,6 +33,11 @@ function Home(props) {
             <div className="my-classes">
                 {favoriteClasses.map((favClass) => 
                     <Class name={favClass} key={favClass}></Class>
+                )}
+            </div>
+            <div className="my-classes">
+                {favoriteClasses.map((favClass) => 
+                    <ClassGraphQL name={favClass} key={favClass}></ClassGraphQL>
                 )}
             </div>
         </div>
